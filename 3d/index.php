0ef91917bc5b8eee79c4a66e0a37de1c8cff68fb
∧° ┐ <we.are.pe.hu@gmail.com>
2022-08-08 19:07:04 +0900

    Update index.php

--- 3d/index.php
+++ 3d/index.php
@@ -158,7 +158,6 @@ fclose($fp);
             height: 0;
             -webkit-transform: translate(-50%, -50%);
             transform: translate(-50%, -50%);
-            max-width: 100vh;
         }
         
         #images .list_item {
