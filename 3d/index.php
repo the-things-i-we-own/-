9d868fc975301c6ad2a99d8e02ad0106b16059fb
∧° ┐ <we.are.pe.hu@gmail.com>
2022-08-09 10:54:21 +0900

    Update index.php

--- 3d/index.php
+++ 3d/index.php
@@ -231,6 +231,10 @@ fclose($fp);
         #sign {
             position: absolute;
             z-index:-1;
+            top:0;
+            left:0;
+            width:100%;
+            height:100%;
         }
         
         #sign .bg,
