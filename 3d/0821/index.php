661e79ca2dbfa619bddd7a6e2a1dd6fb2ddc1d49
∧° ┐ <we.are.pe.hu@gmail.com>
2022-08-15 23:23:53 +0900

    Update index.php

--- 3d/0821/index.php
+++ 3d/0821/index.php
@@ -79,7 +79,7 @@ fclose($fp);
 
 <body>
     <ol class="org">
-        <h2>搬出<br><sup>2022年8月22日(月)</sup></h2>
+        <h2><sup>2022年8月22日(月)</sup><br>搬出</h2>
         <p class="update">
         Last Modified : 
             <?php
